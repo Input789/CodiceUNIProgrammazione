99263ab11fc59b20d09d8eac5ffc097d315a7d6b
Inout789 <matteo.galdini07@gmail.com>
2026-09-23 11:10:13 +0200

    Aggiornamento MigliainKm

--- MigliainKm.cpp
+++ MigliainKm.cpp
@@ -4,10 +4,12 @@ using namespace std;
 #define KM_IN_MILES 1.60934 //Costante non modificabile (per quello in maiuscolo, BestPractice) che converte i miglia in km
 
 int main(){
+    const double KM_IN_MILESSS = 1.60934; //Alternatvia
     double miglia = 0.00;
     cin>>miglia;
 
     cout<<miglia*KM_IN_MILES<<endl;
+    cout<<miglia*KM_IN_MILESSS<<endl; //Alternativa
 
     return 0;
 
